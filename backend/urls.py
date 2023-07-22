e3d1b2852b4147f2421b7c4fa39128662c6c2607
BOTJEFFreal <74595339+BOTJEFFreal@users.noreply.github.com>
2023-07-23 02:19:21 +0530

    changed URL path

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('process/', views.process_data, name='process_data'),
+    path('', views.process_data, name='process_data'),
     path('download-output-csv/', views.download_output_csv, name='download_output_csv'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
