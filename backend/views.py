07e45472f8f868bcf95c48a1813cf842032e7171
BOTJEFFreal <74595339+BOTJEFFreal@users.noreply.github.com>
2023-07-23 17:46:10 +0530

    edit 2

--- backend/views.py
+++ backend/views.py
@@ -1,49 +1,36 @@
 import os
-from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 from .form import DataInputForm
 from .API.Main import execute_code
-
-import os
-from django.conf import settings
-from django.shortcuts import render
-from django.http import HttpResponse
-from .form import DataInputForm
-from .API.Main import execute_code
+import csv
 
 def process_data(request):
     if request.method == 'POST':
         form = DataInputForm(request.POST, request.FILES)
         if form.is_valid():
             # Get the form data
-            file_input = request.FILES['file_input']
+            file_input = request.FILES['file_input'].read()  # Read file content as bytes
             text_input1 = form.cleaned_data['text_input1']
             text_input2 = form.cleaned_data['text_input2']
             text_input3 = form.cleaned_data['text_input3']
             text_input4 = form.cleaned_data['text_input4']
 
-            # Don't save the uploaded input file to the media folder
-
             # Call the execute_code function with the appropriate arguments
-            execute_code(file_input, text_input1, text_input2)
+            csv_content = execute_code(file_input, text_input1, text_input2)
 
-            # Redirect the user to the success page
-            return render(request, 'success.html')
+            # Prepare the CSV response
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="output.csv"'
+
+            # Write the CSV content to the response
+            writer = csv.writer(response)
+            for row in csv_content:
+                writer.writerow(row)
+
+            return response
+
     else:
         form = DataInputForm()
 
     return render(request, 'data_form.html', {'form': form})
-
-
-def download_output_csv(request):
-    # The code for this view remains unchanged
-    output_file_path = request.GET.get('output_file_path')
-    if output_file_path:
-        with open(output_file_path, 'rb') as output_file:
-            response = HttpResponse(output_file.read(), content_type='text/csv')
-            response['Content-Disposition'] = 'attachment; filename="output.csv"'
-            return response
-    else:
-        return render(request, 'error.html', {'error_message': 'Output file path not provided.'})
-
